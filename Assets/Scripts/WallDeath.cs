e8539eece9306dbfcdf599fde80cea72708ba269
RENGOKU\aleks <rengokukedzuro568@gmail.com>
2026-02-26 15:18:06 +0100

    walldeath

--- Assets/Scripts/WallDeath.cs
+++ Assets/Scripts/WallDeath.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
 
 public class WallDeath : MonoBehaviour
 {
-   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetType() == typeof(TilemapCollider2D))
+            {
+            SceneManager.LoadScene(0);
+        }
+
+    }
 
 }
