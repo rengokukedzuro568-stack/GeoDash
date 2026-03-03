22c3eb15e2d15c66e7f60e3f4e0970db426da70a
RENGOKU\aleks <rengokukedzuro568@gmail.com>
2026-03-03 15:32:03 +0100

    Coroutine(Punch())

--- Assets/DashPack/MoveCube.cs
+++ Assets/DashPack/MoveCube.cs
@@ -8,6 +8,8 @@ public class MoveCube : MonoBehaviour
     [SerializeField] private float speed = 5;
     [SerializeField] private float jump = 10;
 
+    public bool canMove = true;
+
     private bool isGround = true;
     private Rigidbody2D rgb;
 
@@ -29,7 +31,11 @@ public class MoveCube : MonoBehaviour
 
     void Update()
     {
-        rgb.velocity = new Vector2(speed, rgb.velocity.y);
+        if (canMove)
+        {
+            rgb.velocity = new Vector2(speed, rgb.velocity.y);
+        }
+        
         if (Input.GetKeyDown(KeyCode.Space) && isGround)
         {
             rgb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
--- Assets/Scripts/WallDeath.cs
+++ Assets/Scripts/WallDeath.cs
@@ -6,13 +6,22 @@ using UnityEngine.Tilemaps;
 
 public class WallDeath : MonoBehaviour
 {
+    [SerializeField] private MoveCube moveCube;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetType() == typeof(TilemapCollider2D))
             {
-            SceneManager.LoadScene(0);
+            StartCoroutine(Punch());
         }
 
     }
-
+    private IEnumerator Punch() 
+    { 
+        moveCube.canMove = false;
+        moveCube.GetComponent<Rigidbody2D>().AddForce(new Vector2 (-15, 15),ForceMode2D.Impulse);
+        yield return new WaitForSecondsRealtime(1.55f);
+        moveCube.canMove = true;
+        SceneManager.LoadScene(0);
+    }
 }
