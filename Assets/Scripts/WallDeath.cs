bae9fa1bdd5a0dcd4f47be83858a38f681a48052
RENGOKU\aleks <rengokukedzuro568@gmail.com>
2026-03-05 15:59:54 +0100

    map

--- Assets/Scripts/WallDeath.cs
+++ Assets/Scripts/WallDeath.cs
@@ -7,10 +7,19 @@ using UnityEngine.Tilemaps;
 public class WallDeath : MonoBehaviour
 {
     [SerializeField] private MoveCube moveCube;
+    [SerializeField] private TilemapRenderer map;
+    [SerializeField] private BoxCollider2D col2D;
 
+    private bool isPunched;
+    private void Start()
+    {
+        col2D.enabled = true;
+        map.enabled = true;
+        isPunched = false;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetType() == typeof(TilemapCollider2D))
+        if (collision.GetType() == typeof(TilemapCollider2D) && !isPunched)
             {
             StartCoroutine(Punch());
         }
@@ -18,9 +27,14 @@ public class WallDeath : MonoBehaviour
     }
     private IEnumerator Punch() 
     { 
+       
         moveCube.canMove = false;
         moveCube.GetComponent<Rigidbody2D>().AddForce(new Vector2 (-15, 15),ForceMode2D.Impulse);
+        map.enabled = false;
+        col2D.enabled = false;
+        isPunched = true;   
         yield return new WaitForSecondsRealtime(1.55f);
+       
         moveCube.canMove = true;
         SceneManager.LoadScene(0);
     }
